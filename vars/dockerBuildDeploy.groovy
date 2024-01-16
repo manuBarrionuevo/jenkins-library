e5bdce0ac0a1f40be00019b70a31e449ba3da5d8
manuelBarrionuevo <manuelbarrionuevo678@hotmail.com>
2024-01-16 13:08:37 -0300

    update

--- vars/dockerBuildDeploy.groovy
+++ vars/dockerBuildDeploy.groovy
@@ -1,34 +1,38 @@
-def dockerBuildDeploy = [:]
+// En dockerBuildDeploy.groovy
 
-dockerBuildDeploy.buildDockerImage = { imageName, version, directory ->
-    dir(directory) {
+def call() {
+    def dockerBuildDeploy = [:]
+
+    dockerBuildDeploy.buildDockerImage = { imageName, version, directory ->
+        dir(directory) {
+            sh """
+                docker build -t $imageName:$version .
+            """
+        }
+    }
+
+    dockerBuildDeploy.pushDockerImage = { imageName, version, directory ->
         sh """
-            docker build -t $imageName:$version .
+            docker push $imageName:$version
         """
     }
-}
 
-dockerBuildDeploy.pushDockerImage = { imageName, version, directory ->
-    sh """
-        docker push $imageName:$version
-    """
-}
+    dockerBuildDeploy.dockerLogin = { registryUrl ->
+        withCredentials([usernamePassword(credentialsId: 'dockerHub', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASSWORD')]) {
+            withDockerRegistry([url: registryUrl]) {
+                return true
+            }
+        }
+        return false
+    }
 
-dockerBuildDeploy.dockerLogin = { registryUrl ->
-    withCredentials([usernamePassword(credentialsId: 'dockerHub', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASSWORD')]) {
-        withDockerRegistry([url: registryUrl]) {
-            return true
+    dockerBuildDeploy.validateDirectories = { directoryList ->
+        directoryList.each { directory ->
+            if (!fileExists(directory)) {
+                error "El directorio '${directory}' no existe."
+            }
         }
     }
-    return false
-}
 
-dockerBuildDeploy.validateDirectories = { directoryList ->
-    directoryList.each { directory ->
-        if (!fileExists(directory)) {
-            error "El directorio '${directory}' no existe."
-        }
-    }
+    return dockerBuildDeploy
 }
-
-return dockerBuildDeploy  
