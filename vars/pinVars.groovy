e62a6b27ac61ed2be40b5e94b4bde4229fa69054
manuelBarrionuevo <manuelbarrionuevo678@hotmail.com>
2024-02-14 20:36:40 -0300

    update

--- vars/pinVars.groovy
+++ vars/pinVars.groovy
@@ -15,19 +15,13 @@ def call() {
             docker push $imageName:$version
         """
     }
-    pinVars.dockerLogin = { registryUrl, credentialsId ->
+    pinVars.dockerLogin = { registryUrl ->
         withCredentials([usernamePassword(credentialsId: 'dockerHub', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASSWORD')]) {
-            echo "Intentando login en Docker Hub con URL: ${registryUrl}"
-            try {
-                withDockerRegistry([url: registryUrl, credentialsId: credentialsId]) {
-                    echo 'Inicio de sesión exitoso en Docker Hub'
-                    return true
-                }
-        } catch (Exception e) {
-                echo "Fallo en el inicio de sesión en Docker Hub: ${e.message}"
-                return false
+            withDockerRegistry([url: registryUrl]) {
+                return true
             }
         }
+        return false
     }
 
     return pinVars
