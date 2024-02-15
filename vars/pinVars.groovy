10c091426ec2f3702e8aa04c3caaf5f7e9e41b45
manuelBarrionuevo <manuelbarrionuevo678@hotmail.com>
2024-02-15 15:17:40 -0300

    update

--- vars/pinVars.groovy
+++ vars/pinVars.groovy
@@ -17,19 +17,12 @@ def call() {
     }
 
     pinVars.dockerLogin = { registryUrl ->
-        withCredentials([usernamePassword(credentialsId: 'dockerHub', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASSWORD')]) { credentials ->
-            if (credentials) {
-                env.DOCKER_USER = credentials.username
-                env.DOCKER_PASSWORD = credentials.password
-
-                withDockerRegistry([url: registryUrl]) {
-                    return true
-                }
-            } else {
-                error 'No se pudieron obtener las credenciales de Docker Hub.'
-                return false
+        withCredentials([usernamePassword(credentialsId: 'dockerHub', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASSWORD')]) {
+            withDockerRegistry([url: registryUrl]) {
+                return true
             }
         }
+        return false
     }
 
     return pinVars
